b96887b7a67a42a830b297db4f72b2d2880c53d4
Casey Loyda <cgloyda@gmail.com>
2018-12-01 18:28:23 -0600

    Updated file structure

---
+++ AoC_2018/src/Day1.java
@@ -0,0 +1,64 @@
+import java.util.HashSet;
+
+public class Day1 {
+	
+	/**
+	 * Solution for Advent of Code 2018 - Day 1
+	 * 
+	 * @param input - String denoting frequency changes
+	 * @return 		- int denoting final frequency (pt1) or first duplicate frequency (pt2)
+	 */
+	public static int solution(String input) {
+		
+		String[] frequencyChangeArray = input.split("\n");				// frequency changes (input)
+		HashSet<Integer> frequenciesReached = new HashSet<Integer>();	// frequencies resulting from changes
+		boolean isFrequencyUnique = true;								// false when frequency reached twice
+		int frequency = 0;												// beginning frequency
+		
+		String prefix = "";												// + or -
+		String number = "";												// amount frequency should change
+		
+		// iterate over frequency change list until a duplicate frequency is found
+		while (isFrequencyUnique) {
+			for (String frequencyChange : frequencyChangeArray) {
+				
+				prefix = frequencyChange.substring(0, 1);
+				number = frequencyChange.substring(1);
+				
+				if (prefix.equals("-")) {
+					frequency -= Integer.parseInt(number);
+				}
+				else {
+					frequency += Integer.parseInt(number);
+				}
+				
+				// hashset doesn't allow duplicate keys, adding returns false
+				isFrequencyUnique = frequenciesReached.add(frequency);
+				
+				if (!isFrequencyUnique) {
+					return frequency;
+				}
+				
+			}
+		}
+		
+		return 0;
+		
+	}
+
+	public static void main(String[] args) {
+		
+		// test input
+		String input = "+7\n"
+				+ "+7\n"
+				+ "-2\n"
+				+ "-7\n"
+				+ "-4";
+		
+		// exercise solution given test input
+		int soln = solution(input);
+
+		System.out.println("Solution Output: " + soln);
+	}
+
+}
---
+++ AoC_2018/src/Day2.java
@@ -0,0 +1,38 @@
+import java.util.HashSet;
+
+public class Day2 {
+
+	/**
+	 * Solution for Advent of Code 2018 - Day 2
+	 * 
+	 * @param input - 
+	 * @return 		- 
+	 */
+	public static int solution1(String input) {
+		
+		return 0;
+		
+	}
+	
+	public static int solution2(String input) {
+		
+		return 0;
+		
+	}
+
+	public static void main(String[] args) {
+		
+		// test input
+		String input = "+7\n"
+				+ "+7\n"
+				+ "-2\n"
+				+ "-7\n"
+				+ "-4";
+		
+		// exercise solution given test input
+		int soln = solution1(input);
+
+		System.out.println("Solution Output: " + soln);
+	}
+
+}
